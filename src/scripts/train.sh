e629677bd19c730f6beee0cfcc049d5fffad69fe
lee-man <minli.amoy@gmail.com>
2020-07-29 17:38:32 +0800

    Update train.sh

--- src/scripts/train.sh
+++ src/scripts/train.sh
@@ -1,9 +1,9 @@
 # main script to train
 
 # IMAGENET_PATH='/media/nthere/datasets/ISLVRC2014_Det/'
-IMAGENET_PATH='/research/dept2/mli/data/vot/small_dataset/ImageNet'
+IMAGENET_PATH='/research/dept2/mli/data/vot/ImageNet'
 # ALOV_PATH='/media/nthere/datasets/ALOV/'
-ALOV_PATH='/research/dept2/mli/data/vot/small_dataset/ALOV'
+ALOV_PATH='/research/dept2/mli/data/vot/ALOV'
 SAVE_PATH='./caffenet/'
 PRETRAINED_MODEL_PATH='../goturn/models/pretrained/caffenet_weights.npy'
 

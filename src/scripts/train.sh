cb5786154e30287f739b4ef86f673551cee8f12d
lee-man <minli.amoy@gmail.com>
2020-07-30 17:16:27 +0800

    Update train.sh

--- src/scripts/train.sh
+++ src/scripts/train.sh
@@ -1,9 +1,9 @@
 # main script to train
 
-IMAGENET_PATH='/research/dept2/mli/data/vot/small_dataset/ImageNet'
-# IMAGENET_PATH='/research/dept2/mli/data/vot/ILSVR2014_DET'
-ALOV_PATH='/research/dept2/mli/data/vot/small_dataset/ALOV/'
-# ALOV_PATH='/research/dept2/mli/data/vot/ALOV'
+IMAGENET_PATH='/data/ssd/public/mli/data/vot/small_dataset/ImageNet'
+# IMAGENET_PATH='/data/ssd/public/mli/data/vot/ILSVR2014_DET'
+ALOV_PATH='/data/ssd/public/data/vot/small_dataset/ALOV/'
+# ALOV_PATH='/data/ssd/public/data/vot/ALOV'
 SAVE_PATH='./caffenet/'
 PRETRAINED_MODEL_PATH='../goturn/models/pretrained/caffenet_weights.npy'
 
